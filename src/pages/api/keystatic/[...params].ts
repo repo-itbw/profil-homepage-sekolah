5fa978c126b06e96288482d99b36cb9c0e4da274
axel <nshxel@gmail.com>
2026-09-02 18:44:05 +0700

    fix: disable prerender for keystatic API route

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -1,3 +1,6 @@
+// PENTING: Matikan prerender agar API Keystatic berjalan secara Serverless (SSR)
+export const prerender = false;
+
 import { makeHandler } from '@keystatic/astro/api';
 import config from '../../../../keystatic.config';
 
