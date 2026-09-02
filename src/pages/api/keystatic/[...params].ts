ebc32512de9bc48db7ca0e34570314181292acbe
axel <nshxel@gmail.com>
2026-09-02 19:44:47 +0700

    fix: pass explicit auth credentials to keystatic api handler

--- src/pages/api/keystatic/[...params].ts
+++ src/pages/api/keystatic/[...params].ts
@@ -6,4 +6,7 @@ import config from '../../../../keystatic.config.ts';
 
 export const ALL = makeHandler({
   config,
+  clientId: process.env.KEYSTATIC_GITHUB_CLIENT_ID || import.meta.env.KEYSTATIC_GITHUB_CLIENT_ID,
+  clientSecret: process.env.KEYSTATIC_GITHUB_CLIENT_SECRET || import.meta.env.KEYSTATIC_GITHUB_CLIENT_SECRET,
+  secret: process.env.KEYSTATIC_SECRET || import.meta.env.KEYSTATIC_SECRET,
 });
